528944ec7b8558571263d9175bfbb323e7814f87
Auxilus <raghavsphadke@gmail.com>
2022-12-25 18:16:15 +0530

    update string.c

--- utils/string.c
+++ utils/string.c
@@ -1,6 +1,8 @@
 #include "string.h"
 #include "mem.h"
 #include <stddef.h>
+#include <stdarg.h>
+#include <stdbool.h>
 
 #define true	1
 #define false 0
@@ -81,26 +83,6 @@ char*  strcat(char* destination, const char* source)
 }
 
 /* TEST: string functions */
-/*
-
-	 Copyright 2009 Pierre KRIEGER
-
-	 Licensed under the Apache License, Version 2.0 (the "License");
-	 you may not use this file except in compliance with the License.
-	 You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-*/
-
-#include <stdarg.h>
-#include <stdbool.h>
 
 int snprintf(char* str, size_t size, const char* format, ...) {
 	va_list ap;
