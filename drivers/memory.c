241fb7c34f0a5ef61d3fb4c4c675478c536b9c44
Auxilus <raghavsphadke@gmail.com>
2023-01-04 11:15:29 +0530

    print detected memory type

--- drivers/memory.c
+++ drivers/memory.c
@@ -10,7 +10,20 @@ void pmm_dump(void)
 
 	for (uint32_t i = 0; i < num; i++) {
 		char *message = (char*)malloc(256);
-		sprintf(message, "MEMORY: region: %u 0x%08x%08x 0x%08x%08x %u\n", i, smaps->BaseH, smaps->BaseL, smaps->LengthH, smaps->LengthL, smaps->Type);
+		char *type = (char*) malloc(16);
+		type = "unidentified";
+
+		switch(smaps->Type) {
+			case 1:
+				type = "Free memory";
+				break;
+			case 2:
+				type = "Reserved memory";
+				break;
+			default:
+				break;
+		}
+		sprintf(message, "MEMORY: region: %u 0x%08x%08x 0x%08x%08x %s\n", i, smaps->BaseH, smaps->BaseL, smaps->LengthH, smaps->LengthL, type);
 		serial_print(message);
 		smaps++;
 	}
