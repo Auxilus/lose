aee5ee7ecfb75ed847a1aa6cf76b7a82b33d052e
Auxilus <raghavsphadke@gmail.com>
2023-01-25 21:35:37 +0530

    use 0x100000 as heap base address

--- utils/mem.h
+++ utils/mem.h
@@ -3,7 +3,7 @@
 
 #include "../cpu/types.h"
 
-static volatile u32 free_mem_addr = 0x10000;
+static volatile u32 free_mem_addr = 0x100000;
 
 void memcpy(u8 *source, u8 *dest, int nbytes);
 int memcmp(const void* ptr1, const void* ptr2, int num);
@@ -13,6 +13,7 @@ void *memmove(void* dest, const void* src, unsigned int n);
 /* At this stage there is no 'free' implemented. */
 u32 kmalloc(int size, int align, u32 *phys_addr);
 void *malloc(int size);
+void *kalloc(int size, int val);
 void free(int size);
 
 #endif
